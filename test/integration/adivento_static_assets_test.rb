562966c98b28060feba8745e650d112d1b845267
Mauro Berlanda <mauro.berlanda@gmail.com>
2026-06-01 12:27:33 +0200

    fix: address sidebar review comments

--- test/integration/adivento_static_assets_test.rb
+++ test/integration/adivento_static_assets_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'digest'
 
 class AdiventoStaticAssetsTest < ActionDispatch::IntegrationTest
   test 'layouts include content-hash query strings for static design assets' do
