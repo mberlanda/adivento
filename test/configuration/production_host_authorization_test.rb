a38d9118ae8b1b62821fd7d9992601750a8357af
Mauro Berlanda <mauro.berlanda@gmail.com>
2026-06-04 09:59:25 +0200

    hotfix: allow cromoswap render host

---
+++ test/configuration/production_host_authorization_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'open3'
+
+class ProductionHostAuthorizationTest < Minitest::Test
+  APP_ROOT = File.expand_path('../..', __dir__)
+
+  def test_production_allows_the_render_cromoswap_hostname
+    host = 'cromoswap.onrender.com'
+    command = "host = #{host.inspect}; abort(\"missing #{host}\") unless Rails.application.config.hosts.include?(host)"
+
+    _stdout, stderr, status = Open3.capture3(
+      { 'RAILS_ENV' => 'production', 'SECRET_KEY_BASE_DUMMY' => '1' },
+      File.join(APP_ROOT, 'bin/rails'),
+      'runner',
+      command,
+      chdir: APP_ROOT
+    )
+
+    assert status.success?, stderr
+  end
+end
